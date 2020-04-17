4b3c4b708d60b790f7e4feee8b139d54a695e540
Hsiao-Te Hu <shoulderhu@gmail.com>
2020-04-17 17:20:01 +0800

    Update demo.sh

--- 0866007_eos_lab5/demo.sh
+++ 0866007_eos_lab5/demo.sh
@@ -3,9 +3,17 @@
 set -e
 
 SESSION="session"
+PORT=4444
 
 make clean
 make
+
+tmux has-session -t $SESSION 2>/dev/null
+
+if [[ $? -ne 0 ]]
+then
+	tmux kill-session -t $SESSION
+fi
 
 tmux new-session -d -s $SESSION
 
@@ -13,12 +21,12 @@ tmux split-window -v -p 75
 tmux split-window -v -p 66
 tmux split-window -v -p 50
 
-tmux send-keys -t 0 './lab5 4444' C-m
+tmux send-keys -t 0 "./lab5 $PORT" C-m
 sleep 1
 
 for i in {1..3}
 do
-	tmux send-keys -t $i 'nc localhost 4444' C-m
+	tmux send-keys -t $i "nc localhost $PORT" C-m
 done
 
 tmux -2 attach-session -t $SESSION
